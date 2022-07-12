3060b8fec482e7ffc2009865b342fc6d96c89ea3
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-07-12 18:18:58 +0300

    feat: parse yaml file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import yaml from 'yaml';
+import path from 'path';
+
 import { AppModule } from './app.module.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rootDirname = path.dirname(__dirname);
+  const DOC_API = await readFile(
+    path.join(rootDirname, 'doc', 'api.yaml'),
+    'utf-8',
+  );
+  const document = yaml.parse(DOC_API);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
