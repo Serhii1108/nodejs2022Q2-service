084ec10927f4fc64e215f06d98afe1fac165b5ce
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-07-28 14:17:34 +0300

    feat: working with real db in favorites module

--- src/modules/favorites/entities/favorite.entity.ts
+++ src/modules/favorites/entities/favorite.entity.ts
@@ -1,9 +1,42 @@
-import { Album } from 'src/modules/albums/entities/album.entity';
-import { Artist } from 'src/modules/artists/entities/artist.entity';
-import { Track } from 'src/modules/tracks/entities/track.entity';
+import { Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 
+import { Album } from '../../albums/entities/album.entity.js';
+import { Artist } from '../../artists/entities/artist.entity.js';
+import { Track } from '../../tracks/entities/track.entity.js';
+
+@Entity('favorite')
 export class Favorite {
+  @PrimaryGeneratedColumn('uuid')
+  id: uuid;
+
+  @ManyToMany(() => Artist, {
+    cascade: ['insert', 'update'],
+    eager: true,
+  })
+  @JoinTable()
   artists: Artist[];
+
+  @ManyToMany(() => Album, {
+    cascade: ['insert', 'update'],
+    eager: true,
+  })
+  @JoinTable()
   albums: Album[];
+
+  @ManyToMany(() => Track, {
+    cascade: ['insert', 'update'],
+    eager: true,
+  })
+  @JoinTable()
   tracks: Track[];
+
+  toResponse() {
+    const { artists, albums, tracks } = this;
+    return { artists, albums, tracks };
+  }
+
+  toUpdate() {
+    const { id, artists, albums, tracks } = this;
+    return { id, artists, albums, tracks };
+  }
 }
--- src/modules/favorites/favorites.module.ts
+++ src/modules/favorites/favorites.module.ts
@@ -1,8 +1,16 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+
 import { FavoritesService } from './services/favorites.service.js';
 import { FavoritesController } from './favorites.controller.js';
 
+import { Favorite } from './entities/favorite.entity.js';
+import { Album } from '../albums/entities/album.entity.js';
+import { Artist } from '../artists/entities/artist.entity.js';
+import { Track } from '../tracks/entities/track.entity.js';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Artist, Album, Track, Favorite])],
   controllers: [FavoritesController],
   providers: [FavoritesService],
 })
